0bc0f8a52c7351378bb29ec9d08ebb6d2c37ec4f
mcampy13 <mcampy13@gmail.com>
2017-05-22 12:19:57 -0400

    Added Logger with toggle record via keypress event

---
+++ Logger/src/CMakeFiles/Logger.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Logger1/src/Logger.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/Logger.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_depth_image.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_depth_image.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_device.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_device.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_device_kinect.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_device_kinect.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_device_oni.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_device_oni.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_device_primesense.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_device_primesense.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_device_xtion.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_device_xtion.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_driver.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_driver.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_exception.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_exception.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_image_bayer_grbg.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_image_bayer_grbg.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_image_rgb24.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_image_rgb24.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_image_yuv_422.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_image_yuv_422.cpp.o"
+  "/home/user/Logger1/src/OpenNI/openni_ir_image.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/OpenNI/openni_ir_image.cpp.o"
+  "/home/user/Logger1/src/main.cpp" "/home/user/Logger1/src/CMakeFiles/Logger.dir/main.cpp.o"
+  "/home/user/Logger1/src/moc_main.cxx" "/home/user/Logger1/src/CMakeFiles/Logger.dir/moc_main.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/ni"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "."
+  "../OpenNI"
+  "/opt/ros/kinetic/include/opencv-3.1.0-dev/opencv"
+  "/opt/ros/kinetic/include/opencv-3.1.0-dev"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Logger/src/CMakeFiles/Logger.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "moc_main.cxx"
+  "CMakeFiles/Logger.dir/main.cpp.o"
+  "CMakeFiles/Logger.dir/moc_main.cxx.o"
+  "CMakeFiles/Logger.dir/Logger.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_driver.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_device.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_exception.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_device_primesense.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_device_kinect.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_device_xtion.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_device_oni.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_image_yuv_422.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_image_bayer_grbg.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_image_rgb24.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_ir_image.cpp.o"
+  "CMakeFiles/Logger.dir/OpenNI/openni_depth_image.cpp.o"
+  "Logger.pdb"
+  "Logger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
